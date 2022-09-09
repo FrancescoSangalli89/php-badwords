98dcf65b246354058703afd702eba9ebf404fa7b
Francesco Sangalli <francesco.sangalli89@gmail.com>
2022-09-09 19:21:54 +0200

    finish: php-badwords

---
+++ getWord.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+
+        $getWord = $_POST['badWord'];
+
+        $censoredWord = str_replace($getWord, '***', "Canta, o dea, l'ira d'Achille Pelide, rovinosa, che infiniti dolori inflisse agli Achei, gettò in preda all'ade molte vite gagliarde d'eroi, ne fece il bottino dei cani");
+
+    ?>
+
+    <p><?php echo $censoredWord ?></p>
+    <p>La lunghezza della stringa è: <?php echo strlen($censoredWord)  ?> caratteri</p>
+
+</body>
+</html>
