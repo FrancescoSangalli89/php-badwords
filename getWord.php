2a42923191f170a275fcc11c5e799254515ea532
Francesco Sangalli <francesco.sangalli89@gmail.com>
2022-09-09 19:29:06 +0200

    created var paragraph

--- getWord.php
+++ getWord.php
@@ -12,12 +12,14 @@
 
         $getWord = $_POST['badWord'];
 
-        $censoredWord = str_replace($getWord, '***', "Canta, o dea, l'ira d'Achille Pelide, rovinosa, che infiniti dolori inflisse agli Achei, gettò in preda all'ade molte vite gagliarde d'eroi, ne fece il bottino dei cani");
+        $paragraph = "Canta, o dea, l'ira d'Achille Pelide, rovinosa, che infiniti dolori inflisse agli Achei, gettò in preda all'ade molte vite gagliarde d'eroi, ne fece il bottino dei cani";
+
+        $censoredWord = str_replace($getWord, '***', $paragraph);
 
     ?>
 
-    <p><?php echo $censoredWord ?></p>
-    <p>La lunghezza della stringa è: <?php echo strlen($censoredWord)  ?> caratteri</p>
+    <p><?php echo $censoredWord?></p>
+    <p>La lunghezza della stringa è: <?php echo strlen($censoredWord)?> caratteri</p>
 
 </body>
 </html>
